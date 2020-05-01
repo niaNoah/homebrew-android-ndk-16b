fb8cb6bf859cf4819ed394217f78c2224af35407
Noah Soliday Bench <noah@nianticlabs.com>
2020-05-01 10:34:57 -0700

    maybe?

--- Formula/android-ndk-16b.rb
+++ Formula/android-ndk-16b.rb
@@ -1,45 +1,23 @@
 class AndroidNdk16b < Formula
-  version '16b'
-  desc ""
+  desc "NDK"
   homepage 'https://developer.android.com/ndk/index.html'
-  url "https://dl.google.com/android/repository/android-ndk-r#{version}-darwin-x86_64.zip"
+  url "https://dl.google.com/android/repository/android-ndk-r16b-darwin-x86_64.zip"
   sha256 "9654a692ed97713e35154bfcacb0028fdc368128d636326f9644ed83eec5d88b"
 
-  conflicts_with cask: 'android-ndk'
-
-  # shim script (https://github.com/caskroom/homebrew-cask/issues/18809)
-  shimscript = "#{staged_path}/ndk_exec.sh"
-  preflight do
-    FileUtils.ln_sf("#{staged_path}/android-ndk-r#{version}", "#{HOMEBREW_PREFIX}/share/android-ndk-r#{version}")
-
-    IO.write shimscript, <<~EOS
-      #!/bin/bash
-      readonly executable="#{staged_path}/android-ndk-r#{version}/$(basename ${0})"
-      test -f "${executable}" && exec "${executable}" "${@}"
-    EOS
-  end
-
-  [
-    'ndk-build',
-    'ndk-depends',
-    'ndk-gdb',
-    'ndk-stack',
-    'ndk-which',
-  ].each { |link_name| binary shimscript, target: link_name }
-
-  uninstall_postflight do
-    FileUtils.rm("#{HOMEBREW_PREFIX}/share/android-ndk-r#{version}")
-  end
-
-  caveats <<~EOS
-    You may want to add to your profile:
-       'export ANDROID_NDK_HOME="#{HOMEBREW_PREFIX}/share/android-ndk-r#{version}"'
-  EOS
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    bin.mkpath
+    prefix.install Dir['*']
+
+    # Create a dummy script to launch the ndk apps
+    ndk_exec = prefix+'ndk-exec.sh'
+    ndk_exec.write <<-EOS.undent
+      #!/bin/sh
+      BASENAME=`basename $0`
+      EXEC="#{prefix}/$BASENAME"
+      test -f "$EXEC" && exec "$EXEC" "$@"
+    EOS
+    ndk_exec.chmod 0755
+    %w[ndk-build ndk-depends ndk-gdb ndk-stack ndk-which].each { |app| bin.install_symlink ndk_exec => app }
   end
 end
