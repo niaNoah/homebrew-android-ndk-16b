39f5932e1b7bade8356e7b2be1e8b44da0be9098
Noah Soliday Bench <noah@nianticlabs.com>
2020-04-30 17:47:27 -0700

    i dont know what i'm doing

--- Formula/android-ndk-16b.rb
+++ Formula/android-ndk-16b.rb
@@ -1,18 +1,16 @@
 class AndroidNdk16b < Formula
   version '16b'
-  sha256 '9654a692ed97713e35154bfcacb0028fdc368128d636326f9644ed83eec5d88b'
-
-  # dl.google.com/android/repository/android-ndk was verified as official when first introduced to the cask
-  url "https://dl.google.com/android/repository/android-ndk-r#{version}-darwin-x86_64.zip"
-  name 'Android NDK r16b'
+  desc ""
   homepage 'https://developer.android.com/ndk/index.html'
+  url "https://dl.google.com/android/repository/android-ndk-r#{version}-darwin-x86_64.zip"
+  sha256 "9654a692ed97713e35154bfcacb0028fdc368128d636326f9644ed83eec5d88b"
 
   conflicts_with cask: 'android-ndk'
 
   # shim script (https://github.com/caskroom/homebrew-cask/issues/18809)
   shimscript = "#{staged_path}/ndk_exec.sh"
   preflight do
-    FileUtils.ln_sf("#{staged_path}/android-ndk-r#{version}", "#{HOMEBREW_PREFIX}/share/android-ndk-r16b")
+    FileUtils.ln_sf("#{staged_path}/android-ndk-r#{version}", "#{HOMEBREW_PREFIX}/share/android-ndk-r#{version}")
 
     IO.write shimscript, <<~EOS
       #!/bin/bash
@@ -30,11 +28,18 @@ class AndroidNdk16b < Formula
   ].each { |link_name| binary shimscript, target: link_name }
 
   uninstall_postflight do
-    FileUtils.rm("#{HOMEBREW_PREFIX}/share/android-ndk-r16b")
+    FileUtils.rm("#{HOMEBREW_PREFIX}/share/android-ndk-r#{version}")
   end
 
   caveats <<~EOS
     You may want to add to your profile:
-       'export ANDROID_NDK_HOME="#{HOMEBREW_PREFIX}/share/android-ndk-r16b"'
+       'export ANDROID_NDK_HOME="#{HOMEBREW_PREFIX}/share/android-ndk-r#{version}"'
   EOS
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+  end
 end
